dc9d315d361fb157a0283485d6307026b443b651
Evan Wright <wrightisevan@gmail.com>
2019-04-02 11:57:33 -0400

    Added templates for functions

--- js/script.js
+++ js/script.js
@@ -1,7 +1,29 @@
 /* This is the js file which will contain javascript functions to sort, filter, and search apps */
 
-jQuery(document).ready(function($) {
-    $(".clickable-row").click(function() {
-        window.location = $(this).data("href");
-    });
-});
+// At the meeting will get clarification about how they want each of these requirements to
+// work and what the parameters should be.
+
+// returns a table sorted by names in alphabetical order
+sortNames {
+	
+}
+
+// returns a table sorted by developers in alphabetical order
+sortDev {
+	
+}
+
+// returns a table sorted by most platforms to lowest
+sortPlatforms { 
+
+}
+
+// returns a table sorted by highest price to lowest
+sortPrice {
+	
+}
+
+// returns a table with all entries whose name match the search
+search {
+	
+}
