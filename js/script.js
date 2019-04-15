1145d007d6efba873e4f40b1e5253b69fdeee8e5
Evan Wright <wrightisevan@gmail.com>
2019-04-15 16:55:17 -0400

    Updated Page w/ Bootstrap

--- js/script.js
+++ js/script.js
@@ -1,29 +1,4 @@
-/* This is the js file which will contain javascript functions to sort, filter, and search apps */
-
-// At the meeting will get clarification about how they want each of these requirements to
-// work and what the parameters should be.
-
-// returns a table sorted by names in alphabetical order
-sortNames {
-	
-}
-
-// returns a table sorted by developers in alphabetical order
-sortDev {
-	
-}
-
-// returns a table sorted by most platforms to lowest
-sortPlatforms { 
-
-}
-
-// returns a table sorted by highest price to lowest
-sortPrice {
-	
-}
-
-// returns a table with all entries whose name match the search
-search {
-	
-}
+$(document).ready(function () {
+$('#repo').DataTable();
+$('.dataTables_length').addClass('bs-select');
+});
